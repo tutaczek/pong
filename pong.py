91c3beb2ce3d3b4915b8559b0f9d1aa9a0b07be9
tutaczek <mangotree69@gmail.com>
2017-11-18 13:52:41 +0100

    background fill

--- pong.py
+++ pong.py
@@ -6,6 +6,7 @@ def main():
     # Initialise screen
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
+    screen.fill((128, 128, 200))
     pygame.display.set_caption('Pong')
     while 1:
         for event in pygame.event.get():
