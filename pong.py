dd8d7faa2f88ed02bfcfe1027e0d09cc25b2862b
tutaczek <mangotree69@gmail.com>
2017-11-19 12:19:56 +0100

    paddle

--- pong.py
+++ pong.py
@@ -5,9 +5,11 @@ import pygame, pygame.locals
 def main():
     # Initialise screen
     pygame.init()
+    pygame.display.set_caption('Pong')
     screen = pygame.display.set_mode((800, 600))
     screen.fill((128, 128, 200))
-    pygame.display.set_caption('Pong')
+    paddle = pygame.Surface((10, 100))
+    screen.blit(paddle, (5, 300))
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
