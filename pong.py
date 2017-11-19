961cea8bd4361fe359c7ae1355277d55155d8f9d
tutaczek <mangotree69@gmail.com>
2017-11-19 13:37:01 +0100

    moving paddle

--- pong.py
+++ pong.py
@@ -7,9 +7,8 @@ def main():
     pygame.init()
     pygame.display.set_caption('Pong')
     screen = pygame.display.set_mode((800, 600))
-    screen.fill((128, 128, 200))
     paddle = pygame.Surface((10, 100))
-    screen.blit(paddle, (5, 300))
+    paddlePosition = 300
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
@@ -17,9 +16,11 @@ def main():
                 return
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    print('UP')
+                    paddlePosition = paddlePosition - 10
                 if event.key == pygame.K_s:
-                    print('DOWN')
+                    paddlePosition = paddlePosition + 10
+        screen.fill((128, 128, 200))    
+        screen.blit(paddle, (5, paddlePosition))
         pygame.display.flip()
 
 main()
