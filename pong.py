3960a7c7d9cd122d09c8e6eda92fd013e0ac9df8
tutaczek <mangotree69@gmail.com>
2017-12-02 12:48:09 +0100

    key.pressed

--- pong.py
+++ pong.py
@@ -2,6 +2,9 @@
 
 import pygame, pygame.locals
 
+WIDTH = 800
+HEIGHT = 600
+
 def main():
     # Initialise screen
     pygame.init()
@@ -11,20 +14,24 @@ def main():
     paddle2 = pygame.Surface((10, 100))
     paddlePosition = 300
     paddlePosition2 = 300
-    while 1:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
                 pygame.quit()
                 return
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    paddlePosition = paddlePosition - 10
-                if event.key == pygame.K_s:
-                    paddlePosition = paddlePosition + 10
-                if event.key == pygame.K_UP:
-                    paddlePosition2 = paddlePosition2 - 10
-                if event.key == pygame.K_DOWN:
-                    paddlePosition2 = paddlePosition2 + 10
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            paddlePosition = paddlePosition - 1
+            if paddlePosition < 0:
+                paddlePosition = 0
+        if keys[pygame.K_s]:
+            paddlePosition = paddlePosition + 1
+            if paddlePosition > 500:
+                paddlePosition = 500
+        if keys[pygame.K_UP]:
+            paddlePosition2 = paddlePosition2 - 1
+        if keys[pygame.K_DOWN]:
+            paddlePosition2 = paddlePosition2 + 1
         screen.fill((128, 128, 200))    
         screen.blit(paddle, (5, paddlePosition))
         screen.blit(paddle2, (785, paddlePosition2))
